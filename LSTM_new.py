b5cc458545d1f04f5e2a80adeb55b1378552a6c2
kk123nnn <32289006+kk123nnn@users.noreply.github.com>
2022-10-23 16:01:43 +1300

    Update LSTM_new.py

--- LSTM_new.py
+++ LSTM_new.py
@@ -1,11 +1,13 @@
 from keras.models import Sequential
+from tensorflow.python.keras.layers import Dropout
 
-class SequeClassifier():
-    def __init__(self, units):
-        self.units = units
-        self.model = None
-    def LSTM_model(self):
-        self.model = Sequential()
-        self.model.add(LSTM(self.units, return_sequences=True))
-        self.model.add(LSTM(self.units))
-        self.model.add(Dense(1, activation='sigmoid'))
+def LSTM_model(input_length):
+
+    model = Sequential()
+    model.add(LSTM(units=50, activation='relu', return_sequences=True, input_shape=(input_length, 1)))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50, activation='relu', return_sequences=False))
+    model.add(Dropout(0.2))
+    model.add(Dense(1, activation='sigmoid'))
+
+    return model
