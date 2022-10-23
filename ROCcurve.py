cf0a5fe92c6655339f5afc23be75860bd51b17dd
kk123nnn <32289006+kk123nnn@users.noreply.github.com>
2022-10-23 13:42:22 +1300

    Add files via upload

---
+++ ROCcurve.py
@@ -0,0 +1,46 @@
+def Draw_ROC(model1,model2,model3, model4, model5, model6):
+
+    fpr_DNN,tpr_DNN,thresholds=roc_curve(np.array(y_eval),model1.predict(X_eval))
+    roc_auc_DNN=auc(fpr_DNN,tpr_DNN)
+
+    fpr_CNN,tpr_CNN,thresholds=roc_curve(np.array(y_eval),model2.predict(X_eval))
+    roc_auc_CNN=auc(fpr_CNN,tpr_CNN)
+
+    fpr_LSTM,tpr_LSTM,thresholds=roc_curve(np.array(y_eval),model3.predict(np.array(X_eval)[:,:, np.newaxis]))
+    roc_auc_LSTM=auc(fpr_LSTM,tpr_LSTM)
+
+    fpr_RNN,tpr_RNN,thresholds=roc_curve(np.array(y_eval),model4.predict(np.array(X_eval).reshape([-1, X_eval.shape[1], 1])))
+    roc_auc_RNN=auc(fpr_RNN,tpr_RNN)
+
+    fpr_RF,tpr_RF,thresholds=roc_curve(np.array(y_eval),model5.predict(np.array(X_eval)))
+    roc_auc_RF=auc(fpr_RF,tpr_RF)
+
+    fpr_SVM,tpr_SVM,thresholds=roc_curve(np.array(y_eval),model6.predict(np.array(X_eval)))
+    roc_auc_SVM=auc(fpr_SVM,tpr_SVM)
+
+
+    plt.figure()
+    lw = 2
+    plt.figure(figsize=(10,10))
+
+    plt.plot(fpr_DNN,tpr_DNN,'purple',lw=lw,label='DNN_AUC = %0.2f'% roc_auc_DNN)
+
+    plt.plot(fpr_CNN,tpr_CNN,color='darkorange', lw=lw, label='CNN_AUC = %0.2f'% roc_auc_CNN)
+
+    plt.plot(fpr_LSTM,tpr_LSTM,color='red', lw=lw, label='LSTM_AUC = %0.2f'% roc_auc_LSTM)
+
+    plt.plot(fpr_RNN,tpr_RNN,color='green', lw=lw, label='RNN_AUC = %0.2f'% roc_auc_RNN)
+
+    plt.plot(fpr_RF,tpr_RF,color='black', lw=lw, label='RF_AUC = %0.2f'% roc_auc_RF)
+
+    plt.plot(fpr_SVM,tpr_SVM,color='brown', lw=lw, label='SVM_AUC = %0.2f'% roc_auc_SVM)
+
+    plt.legend(loc='lower right',fontsize = 12)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.ylabel('True Positive Rate',fontsize = 14)
+    plt.xlabel('Flase Positive Rate',fontsize = 14)
+    plt.title('ROC Curve')
+    plt.savefig('./ROCcurve.eps', dpi=300)
+    plt.show()
+
+Draw_ROC(dnn, cnn, lstm.model, rnn, rf, svc)
---
+++ baxplot.py
@@ -0,0 +1,88 @@
+def performance_plot() -> None:
+    """
+    :return: trains each model using train_model in a batch style and plots the loss over epochs, displays
+             the accuracy metrics
+    """
+
+    models['DNN'].fit(X_train, y_train)
+    models['CNN'].fit(X_train, y_train)
+    models['LSTM'].fit(np.array(X_train)[:,:, np.newaxis], np.array(y_train))
+    models['RNN'].fit(np.array(X_train).reshape([-1, X_train.shape[1], 1]), y_train)
+    models['RF'].fit(X_train.values, y_train.values.ravel())
+    models['SVM'].fit(X_train, y_train.values.ravel())
+
+    print('after training metrics')
+    dnn_metrics = evaluation_metrics(dnn, X_eval, y_eval)
+    cnn_metrics = evaluation_metrics(cnn, X_eval, y_eval)
+    lstm_metrics = evaluation_metrics(models['LSTM'], np.array(X_eval)[:,:, np.newaxis], np.array(y_eval))
+    rnn_metrics = evaluation_metrics(rnn, np.array(X_eval).reshape([-1, X_eval.shape[1], 1]), y_eval)
+    svm_metrics = evaluation_metrics(models['SVM'], X_eval, y_eval)
+    rf_metrics = evaluation_metrics(models['RF'], X_eval, y_eval)
+
+    print('dnn', dnn_metrics)
+    print('cnn', cnn_metrics)
+    print('lstm', lstm_metrics)
+    print('rnn', rnn_metrics)
+    print('svm', svm_metrics)
+    print('rf', rf_metrics)
+
+    plt.figure()
+    fig, ax = plt.subplots()
+    x = np.arange(len(dnn_metrics))
+    width = 0.15  # the width of the bars
+
+    rects1 = ax.bar(x - width, list(dnn_metrics.values()), width, label='DNN')
+    rects2 = ax.bar(x + width , list(cnn_metrics.values()), width, label='CNN')
+    rects3 = ax.bar(x, list(lstm_metrics.values()), width, label='LSTM')
+    rects4 = ax.bar(x - 2 * width, list(rnn_metrics.values()), width, label='RNN')
+    rects5 = ax.bar(x + 2 * width, list(svm_metrics.values()), width=width, label='SVM')
+    rects6 = ax.bar(x - 3 * width, list(rf_metrics.values()), width, label='RF')
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Scores', fontsize=15)
+    ax.set_title('Model Performance metrics', fontsize=15)
+    ax.set_xticks(x, list(dnn_metrics.keys()), fontsize=13, rotation=45)
+    ax.set_ylim(0, 1)
+    ax.legend()
+
+    ax.bar_label(rects1, padding=3)
+    ax.bar_label(rects2, padding=3)
+    ax.bar_label(rects3, padding=3)
+    ax.bar_label(rects4, padding=3)
+    ax.bar_label(rects5, padding=3)
+    ax.bar_label(rects6, padding=3)
+
+    fig.tight_layout()
+
+    #plt.figure()
+    #m = tf.keras.metrics.AUC(curve='ROC')
+    #m.update_state(y_eval, models['DNN'].predict(X_eval))
+
+    #plot_roc_curve(dnn, X_eval, y_eval)
+    plt.savefig('./boxplot.eps', dpi=300)
+    plt.show()
+
+
+
+X_train, X_eval, y_train, y_eval = preprocess_UNSW()
+
+n = X_train.shape[1]
+
+dnn = CustomNN(n, tf.keras.initializers.he_uniform)
+cnn = CNN_Model(n, 2)
+lstm = SequeClassifier(128)
+lstm.build_model()
+rnn = RNN_base()
+rf = RandomForestClassifier()
+svc = SVC(kernel='rbf', cache_size=100, class_weight={0: 0.5, 1: 1}, probability=True, verbose=10)
+
+
+dnn.compile(optimizer='Adam', loss=tf.losses.binary_crossentropy, metrics=[tf.metrics.TruePositives()])
+cnn.compile(loss=tf.losses.binary_crossentropy, optimizer='adam', metrics=[tf.metrics.TruePositives()])
+lstm.model.compile(loss=tf.keras.losses.binary_crossentropy, optimizer='adam', metrics=[tf.metrics.TruePositives()])
+rnn.compile(loss=tf.losses.binary_crossentropy, optimizer='adam', metrics=[tf.metrics.TruePositives()])
+
+models = {'DNN': dnn, 'CNN': cnn, 'LSTM':lstm.model, 'RNN':rnn, "SVM":svc, "RF":rf}
+loss_funcs = {'DNN': log_loss, 'CNN': log_loss, 'LSTM':log_loss, 'RNN':log_loss,  'SVM':log_loss, 'RF':log_loss}
+
+performance_plot()
